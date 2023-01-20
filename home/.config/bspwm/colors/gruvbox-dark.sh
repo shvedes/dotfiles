7e1efaec587916402e8efb8b099a65dfc4593429
shved <shvedkuzmin2@gmail.com>
2023-01-20 23:32:13 +0100

    Update

---
+++ home/.config/bspwm/colors/gruvbox-dark.sh
@@ -0,0 +1,3 @@
+bspc config normal_border_color    "#7c6f64"
+bspc config focused_border_color   "#a06943"
+bspc config active-border_color    "#a06943"
---
+++ home/.config/bspwm/colors/nord.sh
@@ -0,0 +1,4 @@
+bspc config normal_border_color    "#4C566A"
+bspc config focused_border_color   "#81A1C1"
+bspc config active-border_color    "#81A1C1"
+
